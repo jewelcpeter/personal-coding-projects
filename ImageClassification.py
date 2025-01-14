8e635dbb4b8db2961f9defecba8c3b8c3c6f61d1
jewelcpeter <148655835+jewelcpeter@users.noreply.github.com>
2025-01-14 10:55:16 -0600

    Create ImageClassification.py
    classifies images from previous predictions classified

---
+++ ImageClassification.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+# Load CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+# Normalize the pixel values to be between 0 and 1
+x_train, x_test = x_train / 255.0, x_test / 255.0
+# Visualizing some of the training images
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.imshow(x_train[i])
+    plt.title(class_names[y_train[i][0]])
+    plt.axis('off')
+plt.show()
+from tensorflow.keras import layers, models
+
+# Build the CNN Model
+model = models.Sequential([
+    layers.Input(shape=(32, 32, 3)),  # Input layer specifying the shape of the input
+    layers.Conv2D(32, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10)
+])
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+print(f'Test accuracy: {test_acc}')
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0, 1])
+plt.legend(loc='lower right')
+plt.show()
+# Make predictions
+predictions = model.predict(x_test)
+
+# Show the first image and its predicted label
+plt.imshow(x_test[10])
+plt.title(f'Predicted: {class_names[tf.argmax(predictions[10])]}')
+plt.axis('off')
+plt.show()
